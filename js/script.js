eeb8e71c4b2b3761f0284a695db6b52c72c929b6
ValleLuca <valle.lucaAb@outlook.it>
2021-12-02 15:58:52 +0100

    html e js fatto, da aggiungere id

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const namevalue = prompt('Inserisci qui il tuo nome') ;
+
+const surnamevalue = prompt('Inserisci qui il tuo cognome') ;
+
+const colorvalue = prompt('Inserisci qui il tuo colore preferito') ;
+
+const passwordUser = namevalue + surnamevalue + colorvalue + 21 ;
